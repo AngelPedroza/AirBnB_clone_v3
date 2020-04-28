edfd8bf803f587bce53c4acf774637de67f5462f
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2020-04-28 12:02:39 -0500

    [Add] The task 6 for the page not found

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """The main app"""
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 import os
@@ -12,6 +12,11 @@ app.register_blueprint(app_views)
 def close(error):
     """Cole the session"""
     storage.close()
+
+@app.errorhandler(404)
+def not_found(error):
+    """Return this if the request not have a match"""
+    return jsonify({"error": "Not found"}), 404
 
 if __name__ == "__main__":
     port_d = '5000'
